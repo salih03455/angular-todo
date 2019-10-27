08866312aa2b524fb00cf5d1c96235ba2f68952b
salih03455 <salih03455@gmail.com>
2019-10-27 13:42:11 +0300

    datatable çalışır hale getirildi

--- src/app/pages/list-movie/list-movie.module.ts
+++ src/app/pages/list-movie/list-movie.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ListMovieComponent } from './list-movie.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { CdkTableModule } from '@angular/cdk/table';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  imports: [ BrowserModule, MatButtonModule, FormsModule, MatTableModule ],
+  imports: [ BrowserModule, MatButtonModule, FormsModule, CdkTableModule, MatTableModule ],
   declarations: [ ListMovieComponent ]
 })
 export class ListMovieModule {}
