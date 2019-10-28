5d549c6bc4c4c1b0a7545ed34ddbc3d6234cce4a
salih03455 <salih03455@gmail.com>
2019-10-28 16:18:57 +0300

    settings arayüzü tamamlandı

--- src/app/theme-settings/theme-settings.component.ts
+++ src/app/theme-settings/theme-settings.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-theme-settings',
   templateUrl: './theme-settings.component.html',
-  styleUrls: ['./theme-settings.component.scss']
+  styleUrls: ['./theme-settings.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ThemeSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  settings:boolean = false
+
+  ngOnInit() {}
+
+  toggleSettings() {
+    this.settings = !this.settings
   }
 
 }
